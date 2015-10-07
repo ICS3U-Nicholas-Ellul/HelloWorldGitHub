4e77eed7f9580e63150784b2fac06a5865673cf2
Pirates19 <nicholas.ellul@ocsbstudent.ca>
2015-10-07 13:14:33 -0400

    Hello World Basic

---
+++ HelloWorldGitHub/Form1.cs
@@ -0,0 +1,27 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+
+namespace HelloWorldGitHub
+{
+       public partial class frmMainFormHello : Form
+    {
+        public frmMainFormHello()
+        {
+            InitializeComponent();
+        }
+
+        private void label1_Click(object sender, EventArgs e)
+        {
+           
+
+
+        }
+    }
+}
